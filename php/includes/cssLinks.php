9944015e1754accca9d655acfd1c9cfdcdb444b1
VonDenuelle <78338603+VonDenuelle@users.noreply.github.com>
2021-11-03 15:32:16 +0800

    para php include nlng bawat page yang mga yan

---
+++ php/includes/cssLinks.php
@@ -0,0 +1,18 @@
+<!-- Font awesome -->
+<link rel="stylesheet" href="css/font-awesome.min.css">
+<!-- Sandstone Bootstrap CSS -->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<!-- Bootstrap Datatables -->
+<link rel="stylesheet" href="css/dataTables.bootstrap.min.css">
+<!-- Bootstrap social button library -->
+<link rel="stylesheet" href="css/bootstrap-social.css">
+<!-- Bootstrap select -->
+<link rel="stylesheet" href="css/bootstrap-select.css">
+<!-- Bootstrap file input -->
+<link rel="stylesheet" href="css/fileinput.min.css">
+<!-- Awesome Bootstrap checkbox -->
+<link rel="stylesheet" href="css/awesome-bootstrap-checkbox.css">
+<!-- Admin Stye -->
+<link rel="stylesheet" href="./css/style.css">
+<!-- POS Stye -->
+<link rel="stylesheet" href="./css/pos.css">
